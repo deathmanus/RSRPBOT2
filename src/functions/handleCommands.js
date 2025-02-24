ccc03f11195385bc556289e492d2b83fc438b2fa
Death_manus <cerny.alfred@ssakhk.cz>
2025-02-24 11:54:28 +0100

    duplicitní cmd

--- src/functions/handleCommands.js
+++ src/functions/handleCommands.js
@@ -8,15 +8,12 @@ const guildId = '1213225813844037734';
 module.exports = (client) => {
     client.handleCommands = async (commandFolders, path) => {
         client.commandArray = [];
-        
-        // First, delete all existing commands
         const rest = new REST({ version: '9' }).setToken(process.env.token);
         
         try {
-            
-            
+            console.log('Started loading commands...');
 
-            // Load new commands
+            // Load commands
             for (folder of commandFolders) {
                 const commandFiles = fs.readdirSync(`${path}/${folder}`).filter(file => file.endsWith('.js'));
                 for (const file of commandFiles) {
@@ -27,13 +24,13 @@ module.exports = (client) => {
                 }
             }
 
-            // Register only guild commands
-            await rest.put(
+            // Register commands only for the guild
+            const data = await rest.put(
                 Routes.applicationGuildCommands(clientId, guildId),
                 { body: client.commandArray }
             );
 
-            console.log('Successfully registered application commands.');
+            console.log(`Successfully loaded ${data.length} guild commands.`);
             
         } catch (error) {
             console.error('Error:', error);
